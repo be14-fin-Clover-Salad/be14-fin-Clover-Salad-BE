ae049fc6d3cf0b7fb95ed79bdeb64a454a1c189c
jakego1 <jakego13129@gmail.com>
2025-06-24 14:48:00 +0900

    선택된 모든 알림 읽기 기능 추가

--- src/main/java/com/clover/salad/notification/query/controller/NotificationQueryController.java
+++ src/main/java/com/clover/salad/notification/query/controller/NotificationQueryController.java
@@ -1,8 +1,11 @@
 package com.clover.salad.notification.query.controller;
 
+import com.clover.salad.notification.command.application.service.NotificationCommandService;
 import com.clover.salad.notification.query.dto.NotificationDropdownResponseDTO;
 import com.clover.salad.notification.query.dto.NotificationListResponseDTO;
 import com.clover.salad.notification.query.service.NotificationQueryService;
+import com.clover.salad.security.SecurityUtil;
+
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +19,13 @@ import java.util.List;
 public class NotificationQueryController {
 
 	private final NotificationQueryService notificationQueryService;
+	private final NotificationCommandService notificationCommandService;
 
 	@Autowired
-	public NotificationQueryController(NotificationQueryService notificationQueryService
-	) {
+	public NotificationQueryController(NotificationQueryService notificationQueryService,
+		NotificationCommandService notificationCommandService) {
 		this.notificationQueryService = notificationQueryService;
+		this.notificationCommandService = notificationCommandService;
 	}
 
 	/* 설명. 알림 드롭다운 조회 */
