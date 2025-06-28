e86be98cd2b800f32144e82196e4a6c32ab38493
SungMin-hub <sungmin806@gmail.com>
2025-06-28 15:50:00 +0900

    문의사항 테스트 케이스 기본 틀 작성

---
+++ src/test/java/com/clover/salad/qna/command/application/service/QnaCommandServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.clover.salad.qna.command.application.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("문의사항 Command 서비스 계층 테스트")
+class QnaCommandServiceImplTest {
+
+	@Test
+	@DisplayName("문의사항_생성_성공")
+	void createQna() {
+	}
+
+	@Test
+	@DisplayName("문의사항_삭제_성공")
+	void deleteQna() {
+	}
+
+	@Test
+	@DisplayName("문의사항_답변_성공")
+	void answerQna() {
+	}
+
+	@Test
+	@DisplayName("문의사항_답변_수정_성공")
+	void updateAnswer() {
+	}
+}
---
+++ src/test/java/com/clover/salad/qna/query/service/QnaQueryServiceImplTest.java
@@ -0,0 +1,23 @@
+package com.clover.salad.qna.query.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("문의사항 Query 서비스 계층 테스트")
+class QnaQueryServiceImplTest {
+
+	@Test
+	@DisplayName("문의사항_리스트_조회_성공")
+	void findQnaList() {
+	}
+
+	@Test
+	@DisplayName("문의사항_상세보기_조회_성공")
+	void findQnaDetail() {
+	}
+}
